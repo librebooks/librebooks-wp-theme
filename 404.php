1b35d656b3f38e87d57fa2f5e5ec196209c4c032
Heba Fareed <heba@Heba-Laptop.local>
2018-01-25 15:55:15 +0200

    convert all text  to translatable text

--- 404.php
+++ 404.php
@@ -8,40 +8,35 @@
    <div class="notfound_page">
      <div class="post_sec">
         <div class="post_right">
-<h1 class="single_title">الصفحة غير موجودة</h1>
- <div class="article">
-<p>هممم، يبدو أن الصفحة التي تريدها غير موجودة، قد تكون حُذفت أو الرابط الذي اتبعته غير سليم.</p>
-<br />
-<b> حسناً، أين تذهب الآن؟ </b>
-<ul> <li>عد للصفحة <a href="./">الرئيسية</a>.</li>
-<li>بلغ عن المشكلة عبر <a href="/contact-us/">البريد الإلكتروني</a>. </li>
-<li>تابع آخر نشاطات الموقع عبر <a href="/feed/"> خلاصات RSS</a><?php if (get_theme_mod('librebooks_fb_page') != '') { ?>، <a href="https://www.facebook.com/<?php echo get_theme_mod('librebooks_fb_page'); ?>">فيس بوك</a><?php } ?><?php if (get_theme_mod('librebooks_twt_account') != '') { ?>، <a href="https://twitter.com/<?php echo get_theme_mod('librebooks_twt_account'); ?>">تويتر</a><?php } ?>.</li>
-<li>استخدم خاصية <a href="#searchform">البحث</a>.</li>
-</ul>
-</div>
 
-
+          <h1 class="single_title">الصفحة غير موجودة</h1>
+          <div class="article">
+             <?php echo sprintf(__('<p>هممم، يبدو أن الصفحة التي تريدها غير موجودة، قد تكون حُذفت أو الرابط الذي اتبعته غير سليم.</p>
+          <br />
+          <b> حسناً، أين تذهب الآن؟ </b>
+          <ul> <li>عد للصفحة <a href="./">الرئيسية</a>.</li>
+          <li>بلغ عن المشكلة عبر <a href="/contact-us/">البريد الإلكتروني</a>. </li>
+          <li>تابع آخر نشاطات الموقع عبر <a href="/feed/"> خلاصات RSS</a>، <a href="%s">فيس بوك</a>، <a href="%s">تويتر</a>.</li>
+          <li>استخدم خاصية <a href="#searchform">البحث</a>.</li>
+          </ul>', 'LibreBooks'), 'https://www.facebook.com/LibreBooks', 'https://twitter.com/LibreBooksOrg'); ?>
+          </div>
 
         </div><!--//post_right-->
 
         <div class="post_left">
-
              <h1>404</h1>
-
         </div><!--//post_left-->
 
         <div class="book_details">
-        <div id="bookmark"></div>
-             <form method="get" role="search" action="<?php bloginfo('url'); ?>/" id="searchform" title="ابحث في الموقع">
+          <div id="bookmark"></div>
+          <form method="get" role="search" action="<?php bloginfo('url'); ?>/" id="searchform" title="ابحث في الموقع">
 
+            <label class="screen-reader-text" for="s"></label>
+            <input name="s" type="text" size="40" placeholder="" />
+            <input type="submit" id="searchsubmit" value="ابحث" />
 
-          	<label class="screen-reader-text" for="s">
-          	</label>
-                <input name="s" type="text" size="40" placeholder="" />
-        	<input type="submit" id="searchsubmit" value="ابحث" />
-
-	      </form>
-         </div>
+          </form>
+        </div>
      </div><!--//post_sec-->
    </div>
 </div>
--- content-none.php
+++ content-none.php
@@ -3,14 +3,14 @@
     <div class="post_right">
       <h1 class="single_title"><?php echo __('لا توجد كتب', 'LibreBooks'); ?></h1>
       <div class="article">
-        <p>هممم، يبدو أن هذه الصفحة لا تحتوي على كتب!</p>
-        <br />
-        <b> حسناً، أين تذهب الآن؟ </b>
-        <ul> <li>عد للصفحة <a href="./">الرئيسية</a>.</li>
-        <li>بلغ عن المشكلة عبر <a href="/contact-us/">البريد الإلكتروني</a>. </li>
-        <li>تابع آخر نشاطات الموقع عبر <a href="/feed/"> خلاصات RSS</a><?php if (get_theme_mod('librebooks_fb_page') != '') { ?>، <a href="https://www.facebook.com/<?php echo get_theme_mod('librebooks_fb_page'); ?>">فيس بوك</a><?php } ?><?php if (get_theme_mod('librebooks_twt_account') != '') { ?>، <a href="https://twitter.com/<?php echo get_theme_mod('librebooks_twt_account'); ?>">تويتر</a><?php } ?>.</li>
-        <li>استخدم خاصية <a href="#searchform">البحث</a>.</li>
-        </ul>
+        <?php echo sprintf(__('<p>هممم، يبدو أن هذه الصفحة لا تحتوي على كتب!</p>
+     <br />
+     <b> حسناً، أين تذهب الآن؟ </b>
+     <ul> <li>عد للصفحة <a href="./">الرئيسية</a>.</li>
+     <li>بلغ عن المشكلة عبر <a href="/contact-us/">البريد الإلكتروني</a>. </li>
+     <li>تابع آخر نشاطات الموقع عبر <a href="/feed/"> خلاصات RSS</a>، <a href="%s">فيس بوك</a>، <a href="%s">تويتر</a>.</li>
+     <li>استخدم خاصية <a href="#searchform">البحث</a>.</li>
+     </ul>', 'LibreBooks'), 'https://www.facebook.com/LibreBooks', 'https://twitter.com/LibreBooksOrg'); ?>
       </div>
     </div><!--//post_right-->
 
